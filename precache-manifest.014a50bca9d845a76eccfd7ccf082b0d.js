6b1338267c23ada6a98b6fee608494417ee6dc1e
PCMACHERO <pcmacmen@gmail.com>
2019-05-08 01:14:41 -0700

    Updates

---
+++ precache-manifest.014a50bca9d845a76eccfd7ccf082b0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed0aa3d16df68cedb26031eeabadb8b",
+    "url": "/glomar/index.html"
+  },
+  {
+    "revision": "ecd40a3f98f47d5a263a",
+    "url": "/glomar/static/css/2.eb2d33c6.chunk.css"
+  },
+  {
+    "revision": "81b5d37971dbffc43b2d",
+    "url": "/glomar/static/css/main.6551bc30.chunk.css"
+  },
+  {
+    "revision": "ecd40a3f98f47d5a263a",
+    "url": "/glomar/static/js/2.93ed05a0.chunk.js"
+  },
+  {
+    "revision": "81b5d37971dbffc43b2d",
+    "url": "/glomar/static/js/main.4cd56218.chunk.js"
+  },
+  {
+    "revision": "3779eced2dee8d17a425",
+    "url": "/glomar/static/js/runtime~main.ef3783cb.js"
+  },
+  {
+    "revision": "c6545acd5da3717e1344fca9b8e13e24",
+    "url": "/glomar/static/media/bg-stadium.c6545acd.jpg"
+  },
+  {
+    "revision": "0527552bcff3dbb742c96532846ccd61",
+    "url": "/glomar/static/media/glomar-bg-recolored.0527552b.jpg"
+  }
+]);
